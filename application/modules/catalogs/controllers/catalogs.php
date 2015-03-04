102b245635a3ccdc8f6c34998b978360b15d50ca
intelhat <intelhat@gmail.com>
2015-03-04 10:00:48 +0700

    Update catalog

--- application/modules/catalogs/controllers/catalogs.php
+++ application/modules/catalogs/controllers/catalogs.php
@@ -13,12 +13,42 @@ class Catalogs extends Public_Controller {
 		$this->template->build("catalogs/index",@$data);
 	}
 	
-	public function detail() {
+	public function detail($id = false) {
+		if(!$id) {
+			redirect('catalogs');
+		}
+		
+		$data['catalog'] = new Catalog($id);
 		$this->template->build("catalogs/detail",@$data);
 	}
 	
 	public function read_pdf($file_name = null){
 		$data['file_name'] = $file_name;
 		$this->load->view("catalogs/read_pdf",@$data);
+	}
+	
+	public function download_pdf($id = false) {
+		if(!$id) { return false; }
+		
+		$data['file'] = new Catalog($id);
+
+		if(isset($data['file']->path_file) and $data['file']->path_file!="") {
+		    $file = $data['file']->path_file;
+		    $len = filesize($file);
+			$filename = $data['file']->title.'.'.pathinfo( $file , PATHINFO_EXTENSION ) ;
+
+		    header("Pragma: public");
+		    header("Expires: 0");
+		    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+		    header("Cache-Control: public");
+		    header("Content-Description: File Transfer");
+		    header("Content-Type: application/force-download");
+		    $header="Content-Disposition: attachment; filename=".basename($filename).";";
+		    header($header);
+		    header("Content-Transfer-Encoding: binary");
+		    header("Content-Length: ".$len);
+		    @readfile($file);
+		    exit;
+		}
 	}
 }
--- application/modules/catalogs/views/detail.php
+++ application/modules/catalogs/views/detail.php
@@ -1,25 +1,32 @@
 <div class="container container_body">
 	<div class="col-lg-12 header_list">
         <h3 class="page-header">
-        	PORCELAIN GLAZED TILES
-        	<div style="float: right"><a class="btn btn btn-primary">Download <i class="fa fa-download"></i></a></div>
+        	<? echo $catalog->title; ?>
+        	
+        	<? if(!empty($catalog->path_file)) { ?> 
+        		<div style="float: right"><a href="catalogs/download_pdf/<? echo $catalog->id; ?>" class="btn btn btn-primary">Download <i class="fa fa-download"></i></a></div>
+        	<? } ?>
         </h3>
         <div class="col-md-12 breadcrumb">
-        		<a href="index.php">HOME</a> >
-        		<a href="index.php">CATALOGS</a> >
-                <span class="active">PORCELAIN GLAZED TILES</span>
+        	<? echo anchor('', 'HOME').' > '.anchor('catalogs', 'CATALOGS').' > '; ?>
+			<span class="active"><? echo $catalog->title; ?></span>
         </div>
     </div>
     	
-    <div class="col-md-12">
-    	<div class="text-center">
-    		<div>
-    			<iframe src="catalogs/read_pdf/2014 PORCELAIN GLAZED TILES.pdf" style="width: 800px; height: 500px"></iframe>
-    		</div>
-    		
-    	</div>
-    	
-    </div>   
+    
+    <?
+    	if(empty($catalog->path_file)) {
+    		echo '<div style="text-align:center; color:#bbb; height:390px; padding-top:200px;">No files.</div>'; 
+    	} else {
+    	?> 
+		    <div class="col-md-12">
+		    	<div class="text-center">
+		    		<div>
+		    			<iframe src="<? echo $catalog->path_file; ?>" style="width: 800px; height: 500px"></iframe>
+		    		</div>
+		    	</div>
+		    </div>   
+    <? } ?>
     
     <div class="col-md-12"><hr /></div>
 </div>
